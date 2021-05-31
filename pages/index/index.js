ff4ac87f387ebac79871053052b3de41ebd1b1d5
windows8 <1245271811@qq.com>
2021-05-31 13:48:34 +0800

    首页完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,24 +3,30 @@
 const app = getApp()
 
 Page({
-    addLog: (e)=>{//按钮绑定的事件 添加日志到数据库中
-        console.log("add",e.target.dataset.add);
-        const add = e.target.dataset.add;
-        const openid = wx.getStorageSync('openid');
-        if(!openid){//用户没登录时 则转到登录页面
-            wx.switchTab({
-              url: '../../pages/me/me',
-            })
-            return;
-        }
-        wx.cloud.callFunction({
-            name:"createLog",//调用云函数 向数据库添加记录
-            data:{
-                add:add,
-                date:Date.now(),
-                openid:openid
-            }
-        })
+    // addLog: (e)=>{//按钮绑定的事件 添加日志到数据库中
+    //     console.log("add",e.target.dataset.add);
+    //     const add = e.target.dataset.add;
+    //     const openid = wx.getStorageSync('openid');
+    //     if(!openid){//用户没登录时 则转到登录页面
+    //         wx.switchTab({
+    //           url: '../../pages/me/me',
+    //         })
+    //         return;
+    //     }
+    //     wx.cloud.callFunction({
+    //         name:"createLog",//调用云函数 向数据库添加记录
+    //         data:{
+    //             add:add,
+    //             date:Date.now(),
+    //             openid:openid
+    //         }
+    //     })
+    // }
+    click: (e)=>{
+
+    },
+    handleFinish(e) {
+        if (!e.detail) { return }
+        console.log(e.detail);
     }
-    
 })
